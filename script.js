4e7da48af12d8e5d9aace95ea7e1ce143bedeb5a
Dima <dima.tsymbal12345@gmail.com>
2021-12-18 12:58:05 +0200

    Home Work compl

--- script.js
+++ script.js
@@ -6,87 +6,94 @@ class Task {
   }
 }
 
-const input = document.querySelector('.todo-input')
-
+const input = document.querySelector(".todo-input");
 
 class TodoList {
-    constructor(el) {
-      this.todos = [];
-      this.el = el;
+  constructor(el) {
+    this.todos = [];
+    this.findItems = [];
+    this.el = el;
 
-      this.el.addEventListener('click', (e) => {
-        e.preventDefault()
-        let target = e.target
-        let getId = target.parentElement.dataset.id
-       if(target.className === 'completed-button') {
-         this.changeStatus(getId)
-         target.parentElement.classList.toggle('green')
-       } else if(target.className === 'trash-button') {
-         this.removeTodo(getId)
-         target.parentElement.remove()
-       }
-      })
-    }
-    addTodo(todo) {
-      this.todos.push(todo);
-      
-    }
-    removeTodo(id) {
-      this.todos = this.todos.filter((el) => {
-        return el.id !== id;
-      });
-    }
-    getTodos() {
-      return this.todos;
-    }
-    changeStatus(id) {
-      let index = this.todos.findIndex((el) => el.id === id);
-      this.todos[index].status = !this.todos[index].status;
-    }
-    render() {
-      let list = '';
+    this.el.addEventListener("click", (e) => {
+      let target = e.target;
+      let getId = target.parentElement.dataset.id;
+      if (target.className === "completed-button") {
+        this.changeStatus(getId);
+        target.parentElement.classList.toggle("green");
+      } else if (target.className === "trash-button") {
+        this.removeTodo(getId);
+        target.parentElement.remove();
+      }
+    });
+  }
+  addTodo(todo) {
+    this.todos.push(todo);
+  }
+  removeTodo(id) {
+    this.todos = this.todos.filter((el) => {
+      return el.id !== id;
+    });
+  }
+  getTodos() {
+    return this.todos;
+  }
+  changeStatus(id) {
+    let index = this.todos.findIndex((el) => el.id === id);
+    this.todos[index].status = !this.todos[index].status;
+  }
+
+  findTask() {
+    let tasks = this.getTodos();
+    tasks.filter((tasks) => {
+      if (tasks.value.includes(input.value)) {
+        this.findItems.push(tasks);
+      }
+    });
+  }
+
+  render(boolen) {
+    let list = "";
+
+    if (boolen) {
       for (let el of this.todos) {
         if (!el) {
           return;
+        } else {
+          list += `<li data-id="${el.id}" class="todo-item">${el.value}
+              <button class="completed-button"> &#9745; </button>
+              <button class="trash-button"> &#10006; </button> 
+              </li>`;
         }
-        list += `<li data-id="${el.id}" class="todo-item">${el.value}
-            <button class="completed-button"> &#9745; </button>
-            <button class="trash-button"> &#10006; </button> 
-            </li>`
       }
-      this.el.innerHTML = list;
+    } else if (!boolen) {
+      for (let el of this.findItems) {
+        if (!el) {
+          return;
+        } else {
+          list += `<li data-id="${el.id}" class="todo-item">${el.value}
+              <button class="completed-button"> &#9745; </button>
+              <button class="trash-button"> &#10006; </button> 
+              </li>`;
+        }
+      }
     }
-
-    findTask() {
-     return this.todos
-    }
+    this.el.innerHTML = list;
   }
-  
+}
+const list = document.querySelector(".todo-list");
+const main = document.querySelector(".main");
 
-  const list = document.querySelector('.todo-list')
-  const main = document.querySelector('.main')
-  
-  
-  let createTodo = new TodoList(list)
-  let todo = []
+let createTodo = new TodoList(list);
 
-main.addEventListener('click', (e) => {
-    let target = e.target
-    if(target.className === 'todo-button') {
-      todo.push(createTodo.addTodo(new Task(input.value, false)))
-      createTodo.render()
-      input.value = ''
-    }
-
-    else if(target.className === 'search-button') {
-      console.log(createTodo.findTask())
-    }
-  })
-
-
-
-  // // let todo1 = new TodoList(list);
-  // todo1.addTodo(new Task('Footbal', true));
-  // todo1.addTodo(new Task('CSGO', false));
-  // console.log(todo1.getTodos());
-  // todo1.render();
+main.addEventListener("click", (e) => {
+  e.preventDefault();
+  let target = e.target;
+  if (target.className === "todo-button") {
+    createTodo.addTodo(new Task(input.value, false));
+    createTodo.render(true);
+    input.value = "";
+  } else if (target.className === "search-button") {
+    createTodo.findTask();
+    createTodo.render(false);
+  }
+});
